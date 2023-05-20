62e52def82451552e4c25dd67c2b145d08bd4941
caroliv96 <121107004+caroliv96@users.noreply.github.com>
2023-05-20 18:36:43 -0300

    começando ex3

---
+++ Exercicio3/script.js
@@ -0,0 +1,21 @@
+function contar(){
+    var inicio = document.getElementById('inicio')
+    var fim = document.getElementById('fim')
+    var passo = document.getElementById('passo')
+    var numI = inicio.value
+    var numf = fim.value
+    var p = passo.value
+    var res= documet.getElementById('res')
+    if(inicio.value.lenght == 0){
+        res.innerHTML ='Não foi Possível contar!'
+    } else if (passo ==0){
+        
+    }
+
+
+
+
+
+
+
+}
