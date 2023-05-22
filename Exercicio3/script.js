05ce71316e55bfcf72af8727ef1360252b62c600
caroliv96 <121107004+caroliv96@users.noreply.github.com>
2023-05-22 18:09:59 -0300

    exercicio 3

--- Exercicio3/script.js
+++ Exercicio3/script.js
@@ -1,21 +1,33 @@
-function contar(){
-    var inicio = document.getElementById('inicio')
-    var fim = document.getElementById('fim')
-    var passo = document.getElementById('passo')
-    var numI = inicio.value
-    var numf = fim.value
-    var p = passo.value
-    var res= documet.getElementById('res')
-    if(inicio.value.lenght == 0){
-        res.innerHTML ='Não foi Possível contar!'
-    } else if (passo ==0){
+function contar() {
+    var ini = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+    var res =document.getElementById('res')
+    
+    if(ini.value.length == 0 || fim.value.length ==0 || passo.value.length==0){
+        window.alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML ='Contando...'
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        if(p <= 0){
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            //contagem crescente
+            for(var c = i; c <= f; c +=p){
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            // contagem decrescente
+            for (var c = i; c >= f; c -=p)
+            res.innerHTML += ` ${c} \u{1F449}`
+        }
+        res.innerHTML += ` \u{1F3C1}`
         
-    }
-
-
-
-
-
+    } 
 
 
 }
