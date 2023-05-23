967f33bb03212a12066bf9ea5e95d3e871aa8d05
caroliv96 <121107004+caroliv96@users.noreply.github.com>
2023-05-23 16:54:26 -0300

    arrays

---
+++ Aula15/ambiente.js
@@ -0,0 +1,8 @@
+let num =[5,8,9,2,3]
+num.push(7)
+num.sort()
+
+console.log(`Nosso vetor é ${num}`)
+console.log(`Nosso vetor tem ${num.length} posições.`)
+let pos = num.indexOf(8)
+console.log(` O valor 8 está na posição ${pos}`)
